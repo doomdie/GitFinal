6418b90d1a7eb4823f8761d6906771ae0dba187e
Yair <jacklove6p6@gmail.com>
2025-11-06 14:18:42 +0200

    No Transition and JS Code

--- js/code.js
+++ js/code.js
@@ -5,6 +5,10 @@ var currentWidth = parseInt(elBall.style.width)
 var currentHeight = parseInt(elBall.style.height)
 var newWidth = currentWidth + 50
 var newHeight = currentHeight + 50
+if (newWidth >= 400 || newHeight >= 400){
+    newWidth = 100
+    newHeight = 100
+}
 elBall.style.height = newHeight + 'px'
 elBall.style.width = newWidth + 'px'
 }
